4d2b0edffde56c744781cb36d0078c9af387962b
Ashiqur <ashiqur@Ashiqurs-MacBook-Air.local>
2021-08-23 12:50:27 +0530

    Forecast Update

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,14 +1,14 @@
 const request = require('request');
 
 const forecast = (latitude, longitude, callback) => {
-  const url = "http://api.weatherstack.com/current?access_key=5e2adf472f76fe9d162413a7ffd09e68&query="+latitude+","+longitude+"&units=f";
+  const url = "http://api.weatherstack.com/current?access_key=5e2adf472f76fe9d162413a7ffd09e68&query="+latitude+","+longitude+"";
 	request({url, json:true}, (error, { body }) => {
 		if(error) {
 			callback('Cannot connect to weather service', undefined);
 		} else if(body.error) {
 			callback('Unable to find location',undefined);
 		} else {
-			callback(undefined, body.current.weather_descriptions[0] +". It is currently " +body.current.temperature +" degrees out. It feels like " +body.current.feelslike +" degrees out.")
+			callback(undefined, body.current.weather_descriptions[0] +". It is currently " +body.current.temperature +" degrees celcius out. It feels like " +body.current.feelslike +" degrees celcius out. The humidity is "+body.current.humidity+"%.");
 		}
 	})
 }
